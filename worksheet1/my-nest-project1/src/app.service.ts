33501814065690c675516a8e4ff5bc706a5fe949
Toshiakia <toshiakixu@gmail.com>
2024-07-19 15:54:07 +1200

    Update app.service.ts
    helle

--- worksheet1/my-nest-project1/src/app.service.ts
+++ worksheet1/my-nest-project1/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!   this masege is create by jave';
+    return 'Hello World! This message is created by Java';
   }
 }
