887e3263001ae2a29ed887c96748bd57ac02fa01
Toshiakia <toshiakixu@gmail.com>
2024-07-19 15:19:28 +1200

    Update App.js
    change link

--- worksheet1/cise-react-learn/src/App.js
+++ worksheet1/cise-react-learn/src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://my.aut.ac.nz/#/landing"
           target="_blank"
           rel="noopener noreferrer"
         >
